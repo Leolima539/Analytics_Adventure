195215fb8989c45d49e63fc7e399a3558cf0ef8e
Leolima539 <65076406+Leolima539@users.noreply.github.com>
2021-10-16 08:13:44 -0700

    Add files via upload

--- Multiple_Sheet_Stock_Analyzer.vb
+++ Multiple_Sheet_Stock_Analyzer.vb
@@ -1,5 +1,32 @@
 Sub Stocks()
 
+    Sheets.Add.Name = "Combined_Data"
+    Sheets("Combined_Data").Move Before:=Sheets(1)
+    Set combined_sheet = Worksheets("Combined_Data")
+
+    ' Loop through all sheets
+    For Each ws In Worksheets
+
+        ' Find the last row of the combined sheet after each paste
+        ' Add 1 to get first empty row
+        lastRow = combined_sheet.Cells(Rows.Count, "A").End(xlUp).Row + 1
+
+        ' Find the last row of each worksheet
+        ' Subtract one to return the number of rows without header
+        lastRowState = ws.Cells(Rows.Count, "A").End(xlUp).Row - 1
+
+        ' Copy the contents of each state sheet into the combined sheet
+        combined_sheet.Range("A" & lastRow & ":G" & ((lastRowState - 1) + lastRow)).Value = ws.Range("A2:G" & (lastRowState + 1)).Value
+
+    Next ws
+
+    ' Copy the headers from sheet 1
+    combined_sheet.Range("A1:G1").Value = Sheets(2).Range("A1:G1").Value
+    
+    ' Autofit to display data
+    combined_sheet.Columns("A:G").AutoFit
+    'Process Stocks
+
     'Define Variables
     Dim ticker As String
     Dim openPrice As Double
@@ -77,5 +104,15 @@ Sub Stocks()
             Volume = Volume + (Cells(i, 7).Value)
         End If
     Next i
+
+' Hay que ver por que hay que copiar el ticker tb
+    ' maxValue = WorksheetFunction.Max(Columns)
+    ' minValue = WorksheetFunction.Min(Columns)
+    ' Range("P4").Value = WorksheetFunction.Max(Columns(12))
+    ' Range("O2").Value = "Greatest % Increase"
+    ' Range("O3").Value = "Greates % Decrease"
+    ' Range("O4").Value = "Greatest Total Volume"
+    ' Range("O4").Value = Cells(maxValue, 11)
+    ' Range("O4").Value =
 
 End Sub
